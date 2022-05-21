5355fcc4765c06e4bfb4a8fd250c6286ccd1d6e8
forty-twoo <1013417276@qq.com>
2022-05-21 14:07:18 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int a = 1, b = 2;
+int main()
+{
+    int abc = 1;
+    freopen("data.txt", "r", stdin);
+    int c;
+    cin >> c;
+    cout << c + a + b << endl;
+    system("pause");
+}
